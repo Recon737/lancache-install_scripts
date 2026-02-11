3f98e046e545b37e2864fb1d779656220fec2be6
Recon737 <sammaroon@bellsouth.net>
2026-02-10 23:40:29 -0500

    bash translation for the lancachenet/monolithic Dockerfile

--- lancache-3-monolithic.sh
+++ lancache-3-monolithic.sh
@@ -5,3 +5,79 @@
 # https://github.com/lancachenet/monolithic
 
 # Run this script after running lancache-1-ubuntu.sh and lancache-2-ubuntu-nginx.sh
+
+# exit script if any step fails
+set -e
+
+# update, upgrade, and install dependencies
+apt-get update -y && \
+# apt-get upgrade -y && \ # this upgrade is superfluous
+apt-get install jq --no-install-recommends -y
+
+# create env file, add it to .bashrc and activate them
+cat <<EOF >> /root/lancache-monolithic.env
+# LanCache Script 3 - lancachenet/monolithic environment variables
+export GENERICCACHE_VERSION=2 \
+export CACHE_MODE=monolithic \
+export WEBUSER=www-data \
+export CACHE_INDEX_SIZE=500m \
+export CACHE_DISK_SIZE=1000g \
+export MIN_FREE_DISK=10g \
+export CACHE_MAX_AGE=3560d \
+export CACHE_SLICE_SIZE=1m \
+export UPSTREAM_DNS="8.8.8.8 8.8.4.4" \
+export BEAT_TIME=1h \
+export LOGFILE_RETENTION=3560 \
+export CACHE_DOMAINS_REPO="https://github.com/uklans/cache-domains.git" \
+export CACHE_DOMAINS_BRANCH=master \
+export NGINX_WORKER_PROCESSES=auto \
+export NGINX_LOG_FORMAT=cachelog
+export LOG_FORMAT=cachelog
+EOF
+echo "source /root/lancache-monolithic.env" >> ~/.bashrc
+source ~/.bashrc
+
+# clone the github repo and copy overlay directory
+git clone https://github.com/lancachenet/monolithic.git ~/lancachenet-monolithic
+cp -r ~/lancachenet-monolithic/overlay/* /
+
+# delete nginx defaults
+rm /etc/nginx/sites-enabled/* /etc/nginx/stream-enabled/*
+rm /etc/nginx/conf.d/gzip.conf
+
+#create dummy tallylog and set perms
+touch /var/log/tallylog && \
+chmod 754  /var/log/tallylog
+
+# create WEBUSER with disabled login
+id -u ${WEBUSER} &> /dev/null || adduser --system --home /var/www/ --no-create-home --shell /bin/false --group --disabled-login ${WEBUSER} ;\
+
+# create misc directories
+mkdir -m 755 -p /data/cache
+mkdir -m 755 -p /data/info
+mkdir -m 755 -p /data/logs
+mkdir -m 755 -p /tmp/nginx/
+mkdir -m 755 -p /data/cachedomains
+mkdir -m 755 -p /tmp/nginx
+
+# set perms/ownership  
+chmod 755 /scripts/*
+chown -R ${WEBUSER}:${WEBUSER} /data
+
+# create sites-enabled and symlink available sites to enabled directory    
+mkdir -p /etc/nginx/sites-enabled	;\
+ln -s /etc/nginx/sites-available/10_cache.conf /etc/nginx/sites-enabled/10_generic.conf; \
+ln -s /etc/nginx/sites-available/20_upstream.conf /etc/nginx/sites-enabled/20_upstream.conf; \
+ln -s /etc/nginx/sites-available/30_metrics.conf /etc/nginx/sites-enabled/30_metrics.conf; \
+ln -s /etc/nginx/stream-available/10_sni.conf /etc/nginx/stream-enabled/10_sni.conf; \
+
+# clone the cache-domains repo
+git clone --depth=1 --no-single-branch https://github.com/uklans/cache-domains/ /data/cachedomains
+
+# Create volume mount points
+mkdir -p /data/logs /data/cache /data/cachedomains /var/www
+
+# Create /scripts symlink in home directory (WORKDIR replacement)
+ln -s /scripts ~/scripts
+
+echo
